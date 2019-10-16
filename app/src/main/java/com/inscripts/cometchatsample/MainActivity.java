e32c6852079e16eeafd1264c1df830963bd7d6f7
mehtanishant <nishant.mehta@inscripts.in>
2019-10-16 09:13:48 +0530

    Added sample code to create user and updated the gradle dependencies version.

--- app/src/main/java/com/inscripts/cometchatsample/MainActivity.java
+++ app/src/main/java/com/inscripts/cometchatsample/MainActivity.java
@@ -10,16 +10,28 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.inscripts.interfaces.Callbacks;
 import com.inscripts.interfaces.LaunchCallbacks;
+import com.inscripts.keys.CometChatKeys;
 
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import cometchat.inscripts.com.cometchatcore.coresdk.CometChat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String createUserApiEndpoint = "https://api.cometondemand.net/api/v2/createUser";
 
     private String licenseKey = "COMETCHAT-XXXXX-XXXXX-XXXXX-XXXXX"; // Replace the value with your CometChat License Key here
     private String apiKey = "xxxxxxxxxxxxxxxxxxxxxx"; // Replace the value with your CometChat Api Key here
@@ -29,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private CometChat cometChat;
 
 
-    private Button btnLoginSuperHero1, btnLoginSuperHero2, btnLaunchChat, btnInitializeChat;
+    private Button btnLoginSuperHero1, btnLoginSuperHero2, btnLaunchChat, btnInitializeChat, btnCreateUser;
     private ProgressBar pbLoading;
 
 
@@ -48,11 +60,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnLaunchChat = findViewById(R.id.btnLaunchChat);
         btnInitializeChat = findViewById(R.id.btnInitialize);
         pbLoading = findViewById(R.id.pb_loading);
+        btnCreateUser = findViewById(R.id.btnCreateUser);
+
         btnLoginSuperHero1.setOnClickListener(this);
         btnLoginSuperHero2.setOnClickListener(this);
         btnLaunchChat.setOnClickListener(this);
         btnInitializeChat.setOnClickListener(this);
-
+        btnCreateUser.setOnClickListener(this);
     }
 
     @Override
@@ -72,6 +86,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.btnLaunchChat:
                 launchChat();
+
+            case R.id.btnCreateUser:
+                createUser();
+                break;
         }
     }
 
@@ -187,6 +205,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+    }
+
+    /**
+     * Method to create user using restful API and SDK.
+     */
+    private void createUser() {
+
+        // Code to create user via simple volley request
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest request = new StringRequest(Request.Method.POST, createUserApiEndpoint, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e(TAG, "createUser() : success : response : " + response);
+                Toast.makeText(MainActivity.this,response.toString(),Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("createUser() : error : ",error.toString());
+            }
+        }){
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> nameValuePair = new HashMap<>();
+                nameValuePair.put("UID", "UIDComet101");
+                nameValuePair.put("name", "Comet 101");
+                return nameValuePair;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("accept", "application/json");
+                headers.put(CometChatKeys.AjaxKeys.API_KEY, "xxxxxxxxxxxxxxxxxxxxxx");
+                return headers;
+            }
+        };
+        queue.add(request);
+
+        // code to create user using SDK. Please call initializeChat() method first, before using the below code.
+
+        /*cometChat.createUser(MainActivity.this, "UIDComet101", "Comet 101",
+                "", "", "", new Callbacks() {
+                    @Override
+                    public void successCallback(JSONObject jsonObject) {
+                        Log.e(TAG, "createUser() : success : response : " + jsonObject);
+                    }
+
+                    @Override
+                    public void failCallback(JSONObject jsonObject) {
+                        Log.e(TAG, "createUser() : failure : response : " + jsonObject);
+                    }
+                });*/
     }
 
     private void showLoading(boolean show) {
